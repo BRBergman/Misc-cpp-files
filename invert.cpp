3ad6ec49b98f8503cc93ef4fa9cb060e2fcc87c3
BRBergman <50304305+BRBergman@users.noreply.github.com>
2022-06-06 13:36:55 -0400

    Add files via upload

---
+++ invert.cpp
@@ -0,0 +1,27 @@
+void invert(int &val)
+{
+    
+	int temp= val;
+	temp = (temp*-1 );
+
+	val = temp;
+	return;
+}
+
+void invert(float &val)
+{
+	double temp= val;
+	temp = (temp*-1 );
+
+	val = temp;
+	return;
+}
+
+void invert(double &val)
+{
+	double temp= val;
+	temp = (temp*-1 );
+
+	val = temp;
+	return;
+}
---
+++ invert.h
@@ -0,0 +1,14 @@
+#ifndef __INVERT_H__
+#define __INVERT_H__
+
+void invert(int &val);
+
+void invert(float &val);
+
+void invert(double &val);
+
+
+
+
+
+#endif // __INVERT_H__
